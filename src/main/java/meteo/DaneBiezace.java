572590adf79da88bd31f4d0cc6e59e6d5e8b6cc6
student <student@student-VirtualBox>
2012-11-28 11:49:44 +0100

    .

--- src/main/java/meteo/DaneBiezace.java
+++ src/main/java/meteo/DaneBiezace.java
@@ -2,14 +2,27 @@ package meteo;
 
 public class DaneBiezace implements Wyswietlanie {
 
+	private double temperatura;
+	private double wilgotnosc;
+	private double cisnienie;
+	private DanePogodowe danePogodowe;
+	
+	public DaneBiezace(DanePogodowe danePogodowe) {
+		this.danePogodowe = danePogodowe;
+		danePogodowe.dodajWyswietlacz(this);
+	}
+
 	public void aktualizacja(double temp, double wilg, double cis) {
-		// TODO Auto-generated method stub
-		
+		this.temperatura = temp;
+		this.wilgotnosc = wilg;
+		this.cisnienie = cis;
+		wyswietlanie();
 	}
 
 	public void wyswietlanie() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("DaneBiezace: ");	
+		System.out.println("Temperatura = " + temperatura);
+		System.out.println("Wilgotnosc = " + wilgotnosc);
+		System.out.println("Cisnienie = " + cisnienie);
 	}
-
 }
--- src/main/java/meteo/Main.java
+++ src/main/java/meteo/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		DanePogodowe danePogodowe = new DanePogodowe();
-		
 		Meteo meteo = new Meteo(danePogodowe);
-		Statystyki statystyki = new Statystyki(danePogodowe);		
+		Statystyki statystyki = new Statystyki(danePogodowe);	
+		DaneBiezace daneBiezace = new DaneBiezace(danePogodowe);
+		Prognoza prognoza = new Prognoza(danePogodowe);
 		
+		danePogodowe.ustawPomiary(30, 200, 10);
+		danePogodowe.ustawPomiary(30, 200, 10);
 		danePogodowe.ustawPomiary(30, 200, 10);
 	}
 
